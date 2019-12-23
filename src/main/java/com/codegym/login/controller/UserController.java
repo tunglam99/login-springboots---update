2afba671bf3c114fd27541e615dd50082b28b5b2
tunglam99 <lamnthnue.edu.vn@gmail.com>
2019-12-23 11:27:42 +0700

    add controller login

--- src/main/java/com/codegym/login/controller/UserController.java
+++ src/main/java/com/codegym/login/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.codegym.login.controller;
 
+import com.codegym.login.model.JwtResponse;
 import com.codegym.login.model.Role;
 import com.codegym.login.model.User;
 import com.codegym.login.service.RoleService;
 import com.codegym.login.service.UserService;
+import com.codegym.login.service.impl.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,6 +36,8 @@ public class UserController {
 
     @Autowired
     private RoleService roleService;
+    @Autowired
+    private JwtService jwtService;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -57,5 +65,17 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.CREATED);
 
     }
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user){
+        Authentication authentication =authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
 
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtService.generateTokenLogin(authentication);
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        return ResponseEntity.ok(new JwtResponse(jwt,userDetails.getUsername(), userDetails.getAuthorities()));
+
+    }
 }
---
+++ src/main/java/com/codegym/login/model/JwtResponse.java
@@ -0,0 +1,50 @@
+package com.codegym.login.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private String username;
+    private Collection<? extends GrantedAuthority> roles;
+
+    public JwtResponse(String accessToken, String username, Collection<? extends GrantedAuthority> roles) {
+        this.token = accessToken;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Collection<? extends GrantedAuthority> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<? extends GrantedAuthority> roles) {
+        this.roles = roles;
+    }
+}
