7181c38d92d7a0e461dfc7531ff85470aa061a94
tunglam99 <lamnthnue.edu.vn@gmail.com>
2020-01-19 23:09:14 +0700

    create CategoryServiceImpl

---
+++ src/main/java/com/codegym/login/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,39 @@
+package com.codegym.login.service.impl;
+
+import com.codegym.login.model.Category;
+import com.codegym.login.repository.CategoryRepository;
+import com.codegym.login.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public Category findByName(String categoryName) {
+        return categoryRepository.findByName(categoryName);
+    }
+
+    @Override
+    public Iterable<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Optional<Category> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    @Override
+    public void save(Category model) {
+        categoryRepository.save(model);
+    }
+
+    @Override
+    public void remove(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
