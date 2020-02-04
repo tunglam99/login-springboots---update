cd9f05f3eb706a112f315633bc124da9bb1d28d9
tunglam99 <lamnthnue.edu.vn@gmail.com>
2020-02-04 21:37:12 +0700

    create CorrectAnswerServiceImpl

---
+++ src/main/java/com/codegym/login/service/impl/CorrectAnswerServiceImpl.java
@@ -0,0 +1,38 @@
+package com.codegym.login.service.impl;
+
+import com.codegym.login.model.CorrectAnswer;
+import com.codegym.login.model.Question;
+import com.codegym.login.repository.CorrectAnswerRepository;
+import com.codegym.login.service.CorrectAnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class CorrectAnswerServiceImpl implements CorrectAnswerService {
+    @Autowired
+    private CorrectAnswerRepository correctAnswerRepository;
+    @Override
+    public Iterable<CorrectAnswer> findAllByQuestion(Question question) {
+        return correctAnswerRepository.findAllByQuestion(question);
+    }
+
+    @Override
+    public Iterable<CorrectAnswer> findAll() {
+        return correctAnswerRepository.findAll();
+    }
+
+    @Override
+    public Optional<CorrectAnswer> findById(Long id) {
+        return correctAnswerRepository.findById(id);
+    }
+
+    @Override
+    public void save(CorrectAnswer model) {
+        correctAnswerRepository.save(model);
+    }
+
+    @Override
+    public void remove(Long id) {
+        correctAnswerRepository.deleteById(id);
+    }
+}
